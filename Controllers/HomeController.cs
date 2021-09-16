5d31dd923cfd177856f8c0fb1bd7efbe350eee24
Farista Latuconsina <faristalatuconsina@gmail.com>
2021-09-16 19:10:32 +0700

    [feat] Basics

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace basic_auth.Controllers
@@ -14,14 +18,35 @@ namespace basic_auth.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Secret()
         {
-
+            return View();
         }
 
         public IActionResult Authenticate()
         {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "bob@gmail.com"),
+                new Claim("Grandma.Says", "Very nice boi.")
+            };
 
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim("Driving License", "A+")
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "License Identity");
+
+            var userPrinciple = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrinciple);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,13 @@ namespace basic_auth
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("CookieAuth")
+                    .AddCookie("CookieAuth", config =>
+                    {
+                        config.Cookie.Name = "Grandmas.Cookie";
+                        config.LoginPath = "/Home/Authenticate";
+                    });
+
             services.AddControllersWithViews();
         }
 
@@ -27,7 +34,11 @@ namespace basic_auth
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
