527504fa8a2d743ab31725eb7d7581f33bab59b9
Farista Latuconsina <faristalatuconsina@gmail.com>
2021-09-16 18:45:33 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basic_auth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Secret()
+        {
+
+        }
+
+        public IActionResult Authenticate()
+        {
+
+        }
+    }
+}
